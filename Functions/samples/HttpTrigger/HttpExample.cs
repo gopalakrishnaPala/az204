d1c462ce6b735b56102c20784632058afe2bd484
Gopalakrishna Pala <45391916+gopalakrishnaPala@users.noreply.github.com>
2023-08-27 20:56:23 +0530

    Added a http triggered function

---
+++ Functions/samples/HttpTrigger/HttpExample.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace HttpTrigger
+{
+    public class HttpExample
+    {
+        private readonly ILogger _logger;
+
+        public HttpExample(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HttpExample>();
+        }
+
+        [Function("HttpExample")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var queryParams = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
+            var name = queryParams["name"] ?? "Guest";
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString($"Hello {name}, Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
